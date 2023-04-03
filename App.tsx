ab435a2e5a9cce9d87a94a5838899c1ae2133018
Sonam Choeda <sonam.choeda@selise.ch>
2023-04-03 13:21:56 +0600

    initial setup

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './src/pages/home';
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+     <NavigationContainer>
+    <Stack.Navigator>
+    <Stack.Screen name="Home" component={Home} />
+  </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
---
+++ src/pages/home.tsx
@@ -0,0 +1,16 @@
+import { Box } from 'native-base';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Home = ({navigation}: any) => {
+    return (
+       <>
+       <Text>hello</Text>
+       {/* <Box position="absolute" bottom={0}>
+                    <NavigateButton navigation={navigation}/>
+                </Box> */}
+       </> 
+    );
+}
+
+export default Home;
